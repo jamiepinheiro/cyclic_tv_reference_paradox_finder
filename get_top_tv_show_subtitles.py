3417f334af6d7c4142aee160906813513e628b5c
Jamie Pinheiro <jamiepinheiro@gmail.com>
2022-05-22 15:17:09 -0400

    Remove threading (was hitting rate limit)

--- get_top_tv_show_subtitles.py
+++ get_top_tv_show_subtitles.py
@@ -1,6 +1,5 @@
 import requests
 import os
-from threading import Thread
 from babelfish import Language
 from subliminal import download_best_subtitles, Episode
 
@@ -21,20 +20,19 @@ def download_subtitles(series, season, episode):
             with open(file_name, 'wb') as f:
                 f.write(subtitle[0].content)
 
-for i in range(1):
-    params = {'api_key' : TMDB_API_KEY, 'language': 'en-US', 'page': i + 1}
-    r = requests.get(url = POPULAR_TV_SHOWS_URL, params = params)
-    popular_tv_show = r.json()
-    for tv_show in popular_tv_show['results']:
-        params = {'api_key' : TMDB_API_KEY}
-        r = requests.get(url = TV_URL % tv_show['id'], params = params)
-        tv_show = r.json()
-        for season in tv_show['seasons']:
-            threads = []
-            for e in range(season['episode_count']):
-                # +1 as seasons and episodes are generally 1 indexed
-                thread = Thread(target=download_subtitles, args=(tv_show['name'], season['season_number'] + 1, e + 1))
-                threads.append(thread)
-                thread.start()
-            for t in threads:
-                t.join()
+def get_top_tv_show_subtitles():
+    for i in range(1):
+        params = {'api_key' : TMDB_API_KEY, 'language': 'en-US', 'page': i + 1}
+        r = requests.get(url = POPULAR_TV_SHOWS_URL, params = params)
+        popular_tv_show = r.json()
+        for tv_show in popular_tv_show['results']:
+            params = {'api_key' : TMDB_API_KEY}
+            r = requests.get(url = TV_URL % tv_show['id'], params = params)
+            tv_show = r.json()
+            for season in tv_show['seasons']:
+                threads = []
+                for e in range(season['episode_count']):
+                    # +1 as seasons and episodes are generally 1 indexed
+                    download_subtitles(tv_show['name'], season['season_number'] + 1, e + 1)
+
+get_top_tv_show_subtitles()
