05fc0bf665d541509dc3d5c67734a9f790ce894e
Jamie Pinheiro <jamiepinheiro@gmail.com>
2022-05-22 17:59:51 -0400

    Add more debug statments

--- get_top_tv_show_subtitles.py
+++ get_top_tv_show_subtitles.py
@@ -20,18 +20,15 @@ def download_subtitles(series, season, episode):
     for subtitle in subtitles.values():
         with open(file_name, 'wb') as f:
             if subtitle:
+                print("Found")
                 f.write(subtitle[0].content)
 
 def get_top_tv_show_subtitles():
-    for i in range(1):
+    for i in range(100):
         params = {'api_key' : TMDB_API_KEY, 'language': 'en-US', 'page': i + 1}
         r = requests.get(url = POPULAR_TV_SHOWS_URL, params = params)
         popular_tv_show = r.json()
         for tv_show in popular_tv_show['results']:
-            series_file_name = '%s/%s' % (DIR, tv_show['name'])
-            if os.path.isfile(series_file_name):
-                continue
-
             params = {'api_key' : TMDB_API_KEY}
             r = requests.get(url = TV_URL % tv_show['id'], params = params)
             tv_show = r.json()
@@ -39,8 +36,5 @@ def get_top_tv_show_subtitles():
                 for e in range(season['episode_count']):
                     # +1 as seasons and episodes are generally 1 indexed
                     download_subtitles(tv_show['name'], s + 1, e + 1)
-            
-            with open(series_file_name, 'wb') as f:
-                continue
 
 get_top_tv_show_subtitles()
