e3ab8ab7d94aae139cc86edfebdbcf0b77357e7b
Jamie Pinheiro <jamiepinheiro@gmail.com>
2022-05-23 19:14:05 -0400

    Stop looking once empty season hit

--- get_top_tv_show_subtitles.py
+++ get_top_tv_show_subtitles.py
@@ -14,7 +14,8 @@ def download_subtitles_for_season(series, season, num_episode):
     marker_file = '%s/%s_S%s' % (SUBTITLE_DIR, series, season)
     print("Getting %s" % marker_file)
     if os.path.isfile(marker_file):
-        return
+        with open(marker_file, 'r') as f:
+            return f.read() != '0'
 
     start_time = time.time()
 
@@ -28,8 +29,10 @@ def download_subtitles_for_season(series, season, num_episode):
                 f.write(subs[0].content)
 
     subtitles_found = len([s for s in subtitles.values() if s])
-    with open(marker_file, 'w'):
+    with open(marker_file, 'w') as f:
+        f.write(str(subtitles_found))
         print("Found %d subtitles in %fs" % (subtitles_found, time.time() - start_time))
+        return subtitles_found != 0
 
 def get_top_tv_show_subtitles():
     for i in range(1000000):
@@ -42,7 +45,9 @@ def get_top_tv_show_subtitles():
             tv_show = r.json()
             for s, season in enumerate(tv_show['seasons']):
                 # +1 as seasons and episodes are generally 1 indexed
-                found_subs = download_subtitles_for_season(tv_show['name'], s + 1, season['episode_count'])
+                subs_found = download_subtitles_for_season(tv_show['name'], s + 1, season['episode_count'])
+                if not subs_found:
+                    break 
 
 def main():
     if not os.path.exists(SUBTITLE_DIR):
