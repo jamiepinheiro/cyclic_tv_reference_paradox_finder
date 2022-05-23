77160de964f277d4931bf1a7053c0ba3b6a998c7
Jamie Pinheiro <jamiepinheiro@gmail.com>
2022-05-22 21:57:19 -0400

    Add script to build indices

---
+++ index_srts.py
@@ -0,0 +1,69 @@
+import pysrt
+import os
+from whoosh import index
+from whoosh.fields import Schema, TEXT, NUMERIC
+from whoosh.analysis import FancyAnalyzer
+
+from whoosh.qparser import QueryParser
+
+SUBTITLES_DIR = 'subtitles'
+INDEX_DIR = 'index'
+INDEX_MARKER_DIR = 'index_marker'
+
+def get_index():
+    if not os.path.exists(INDEX_MARKER_DIR):
+        os.mkdir(INDEX_MARKER_DIR)
+
+    # check if there is an existing index
+    if os.path.exists(INDEX_DIR):
+        return index.open_dir(INDEX_DIR)
+    # no index, setup a new one
+    else:
+        os.mkdir(INDEX_DIR)
+
+        schema = Schema(
+                    title=TEXT(stored=True),
+                    season=TEXT(stored=True),
+                    episode=TEXT(stored=True),
+                    text=TEXT(stored=True, phrase=True, analyzer=FancyAnalyzer()),
+                    start_time=TEXT(stored=True),
+                    end_time=TEXT(stored=True)
+                )
+        return index.create_in(INDEX_DIR, schema)
+
+def write_srt_to_index(ix, srt):
+    print('Indexing %s' % srt)
+
+    srt_file = '%s/%s' % (SUBTITLES_DIR, srt)
+    
+    # check if already added to index
+    marker_file = '%s/%s' % (INDEX_MARKER_DIR, srt)
+    if os.path.isfile(marker_file):
+        return
+
+    srt_name_sections = srt.split('_')
+    subs = pysrt.open(srt_file, encoding='iso-8859-1')
+
+    writer = ix.writer()
+    for sub in subs:
+        writer.add_document(
+            title=srt_name_sections[0],
+            season=srt_name_sections[1],
+            episode=srt_name_sections[2],
+            text=sub.text,
+            start_time='%d:%d:%d,%d' % (sub.start.hours, sub.start.minutes, sub.start.seconds, sub.start.milliseconds),
+            end_time='%d:%d:%d,%d' % (sub.end.hours, sub.end.minutes, sub.end.seconds, sub.end.milliseconds),
+        )
+    writer.commit()
+
+    with open(marker_file, 'wb') as f:
+        return
+
+def main():
+    ix = get_index()
+
+    for file in os.listdir(SUBTITLES_DIR):
+        if '.srt' in file:
+            write_srt_to_index(ix, file)
+
+main()
