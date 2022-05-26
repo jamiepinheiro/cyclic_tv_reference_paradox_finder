35221e8879bdad8d8b72f4e463bf2dd2b5f7bfce
Jamie Pinheiro <jamiepinheiro@gmail.com>
2022-05-26 09:33:27 -0400

    Dedup cycles

--- web_app/src/App.tsx
+++ web_app/src/App.tsx
@@ -37,10 +37,11 @@ function App() {
           .reduce((prev, curr) => prev + (prev != "" ? ":" : "") + curr, "");
       }
 
-      // Not optimal, doing Nx the amount of work needed, but while N is small, this is fine.
+      // Not optimal, doing N times the amount of work needed, but while N is small, this is fine.
       // If this becomes slow, checkout https://stackoverflow.com/questions/546655/finding-all-cycles-in-a-directed-graph
       function getCycles(tvShows: Map<string, TvShow>) {
         const cycles: string[][] = [];
+        const dedupCycles: string[] = [];
 
         function dfsForCycles(title: string, visited: string[]) {
           const referencesTo = Array.from(
@@ -55,11 +56,21 @@ function App() {
             if (index != -1 && index != 0) {
               return;
             }
-            visited.push(referenceTitle);
             if (index == -1) {
+              visited.push(referenceTitle);
               dfsForCycles(referenceTitle, visited);
             } else if (index == 0) {
+              const path = visited.reduce((prev, curr) => prev + curr, "");
+              const isDupe = dedupCycles.find(c => c.includes(path));
+              if (isDupe) {
+                return;
+              }
+              visited.push(referenceTitle);
               cycles.push([...visited]);
+
+              // Concat the path twice to see if cycles are the same.
+              // ie. 'BCAB' is in the same 'ABCA', so add 'ABCABC' to dedup cycles, 'BCA' will be a substring
+              dedupCycles.push(path + path);
             }
             visited.pop();
           });
